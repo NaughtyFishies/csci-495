484ad4d175bc67d72570246a6838103dc6ef1292
Warren MacEvoy <wmacevoy@gmail.com>
2025-03-07 10:14:38 -0700

    hello

--- cuda/hello.cu
+++ cuda/hello.cu
@@ -1,11 +1,18 @@
 #include <iostream>
 
-__global__ void helloCUDA() {
-    printf("Hello from thread %d\n", threadIdx.x);
+__global__ void helloCUDA(int numHellos) {
+    int hello = blockIdx.x * blockDim.x + threadIdx.x;
+    if (hello < numHellos) {
+        printf("Hello # %d\n", hello);
+    }
 }
 
 int main() {
-    helloCUDA<<<1, 10>>>(); // 1 block, 10 threads
+    dim3 threadsPerBlock(10);
+    const int numHellos = 10;
+    dim3 numBlocks((numHellos+threadsPerBlock.x-1)/threadsPerBlock.x)
+
+    helloCUDA<<<numBlocks,threadsPerBlock>>>(numHellos); // 1 block, 10 threads
     cudaDeviceSynchronize();
     return 0;
 }
