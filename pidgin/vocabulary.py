d1521e3354f2905ad84de240d346337c5a127824
Kyle Remmenga <kyleremmenga@gmail.com>
2025-04-17 13:01:05 -0600

    added more stories and more vocab

--- pidgin/vocabulary.py
+++ pidgin/vocabulary.py
@@ -8,20 +8,24 @@ vocabulary = {
         "think", "use", "are", "get", "fly", "sleep", "stand", "shine", "like",
         "come", "give", "eat", "play", "wake", "run", "thank", "dream", "wave",
         "sing", "sit", "listen", "bark", "blow", "go", "roll", "look", "laugh",
-        "drink", "can", "again"
+        "drink", "can", "again", "call", "have", "swing", "fall", "walk", "help",
+        "climb", "kick", "cook", "stop", "wait", "hit", "wag", "hear", "rise",
+        "hurt", "jump", "meow", "need", "twinkle", "tired", "throw", "purr",
+        "live", "rain", "is", "away", "turn", "smell", "ask", "find", "catch",
+        "drop", "lick", "try", "stay", "push", "hold", "buy"
     ],
     
     # Locations
     "locations": [
         "right", "left", "front", "back", "north", "south", "east", "west",
-        "here", "there", "up", "down"
+        "here", "there", "up", "down", "far", "out"
     ],
     
     # Time
-    "time": ["now", "last", "next", "old", "new"],
+    "time": ["now", "last", "next", "old", "new", "soon", "morning", "tomorrow"],
     
     # Grammar markers
-    "grammar": ["ha", "ed", "will", "ing"],
+    "grammar": ["ha", "ed", "will", "ing", "then", "but", "for", "my", "this"],
     
     # Logic words
     "logic": ["no", "yes", "and", "or"],
@@ -36,18 +40,26 @@ vocabulary = {
     "colors": ["white", "black", "grey", "red", "green", "blue", "yellow"],
     
     # Prepositions
-    "prepositions": ["in", "on", "at", "with", "near", "too"],
+    "prepositions": ["in", "on", "at", "with", "near", "too", "to", "of"],
     
     # Nouns
     "nouns": [
         "food", "water", "house", "person", "child", "parent", "tree", "sun",
         "moon", "sky", "bird", "dog", "cat", "road", "day", "night", "end",
-        "begin", "fruit", "wind", "both", "ball", "star", "wing", "brother", "sister", "mom", "dad"
+        "begin", "fruit", "wind", "both", "ball", "star", "wing", "brother", "sister", "mom", "dad",
+        "home", "school", "forest", "store", "friend", "stick", "apple", "time", "bed", "hand",
+        "door", "table", "park", "playground", "leaf", "tail", "bread", "bush", "car", "grass",
+        "smile", "name", "town", "bike", "slide", "teacher", "ground", "foot", "us", "he", "her",
+        "rock", "money", "title", "kid", "song", "branch", "mouth", "boy", "nest", "she", "them",
+        "leg", "rope", "fish", "friends", "shop"
+
     ],
     
     # Adjectives
     "adjectives": [
-        "big", "small", "good", "bad", "hot", "cold", "light", "dark"
+        "big", "small", "good", "bad", "hot", "cold", "light", "dark", "fast", "after",
+        "little", "more", "high", "away", "safe", "fun", "sleepy", "mine", "sudden",
+        "soft", "bright", "sweet", "cool", "loud"
     ],
     
     # Change state verbs
@@ -57,5 +69,7 @@ vocabulary = {
     "internal_state": ["hungry", "full", "happy", "sad", "mad", "scared", "thirsty"],
     
     # Names
-    "names": ["al", "bu", "ku", "bur", "gur", "sur", "gu", "ret", "pet", "pur", "yur", "gee", "zee", "mom", "dad"]
+    "names": ["al", "bu", "ku", "bur", "gur", "sur", "gu", "ret", "pet", "pur", "yur", "gee", "zee", "mom", "dad", "sis", "bro"],
+    # Expressions
+    "expressions": ["hi", "oh", "ok", "bye", "hello", "maybe", "how", "hey"]
 }
